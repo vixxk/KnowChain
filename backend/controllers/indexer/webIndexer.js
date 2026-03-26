5cc64f3b810d9243c53522f58193fdd80bfa9756
vixxk <kamjorkismatwala@gmail.com>
2026-03-26 22:35:07 +0530

    web indexing fix

--- backend/controllers/indexer/webIndexer.js
+++ backend/controllers/indexer/webIndexer.js
@@ -5,7 +5,17 @@ import { getEmbeddings } from "../../utils/ai.js";
 import { chunkArray, runIncrementalIndexing } from "./base.js";
 
 export async function webIndexer(url, collectionName) {
-  const response = await axios.get(url, { headers: { 'User-Agent': 'Mozilla/5.0' }});
+  const response = await axios.get(url, { 
+    headers: { 
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+      'Accept-Language': 'en-US,en;q=0.9',
+      'Accept-Encoding': 'gzip, deflate',
+      'Connection': 'keep-alive',
+      'Upgrade-Insecure-Requests': '1',
+      'Cache-Control': 'max-age=0'
+    }
+  });
   const $ = load(response.data);
   $('nav, footer, aside, header, script, style').remove();
   
